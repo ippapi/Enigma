cdb207a3f08733766b7f830b25c77ef3e308417f
4ntisocialband <4ntisocial@gmail.com>
2025-02-20 15:44:44 +0700

    add database's messages schema

---
+++ Enigma_app/src/models/messages.js
@@ -0,0 +1,11 @@
+import mongoose, { mongo } from "mongoose";
+import { type } from "os";
+
+const MessageSchema = new mongoose.Schema({
+    user: {type: String, required: true},
+    message: {type: String, required: true},
+    timestamp: {type: Date, default: Date.now},
+});
+
+// If not exist, use the MessageSchema, else use the existed one
+export default mongoose.models.Message || mongoose.model("Message", MessageSchema);
