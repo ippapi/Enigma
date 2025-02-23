780c8c0d5c92cd0a821d7465a0c3dcd00ee99439
4ntisocialband <4ntisocial@gmail.com>
2025-02-23 08:30:41 +0700

    update socket io, add comment

--- Enigma_app/src/lib/socket.js
+++ Enigma_app/src/lib/socket.js
@@ -1,26 +1,42 @@
+/* 
+    Create function to initialize socket
+
+    - input: httpServer: initialized server to create socket
+
+    - output: create and return socket
+*/
+
 import { Server } from "socket.io";
 
 let io;
 
 const initializeSocket = (httpServer) => {
+    const socket_url = process.env.NEXT_PUBLIC_SOCKET_URL;
+    
     if (!io) {
         io = new Server(httpServer, {
-            cors: { origin: "*" },
+            cors: { 
+                origin: socket_url,
+                credentials: true,
+            },
             transports: ["websocket"],
         });
 
-        io.on("connection", (socket) => {
-            // Log user connection
-            console.log("User connected:", socket.id);
+        io.on("connection", (socket) => {        
+            // Log user disconnection
+            console.log("User connected");
 
             // Listen to client send message event
             socket.on("sendMessage", (message) => {
-                io.emit("messageReceived", message); // Emit that message to all client
+                io.emit("messageReceived", {
+                    user: message.user,
+                    message: message.message,
+                }); // Emit that message to all client
             });
 
-            // Loc user disconnection
+            // Log user disconnection
             socket.on("disconnect", () => {
-                console.log("User disconnected:", socket.id);
+                console.log("User disconnected!");
             });
         });
     }
