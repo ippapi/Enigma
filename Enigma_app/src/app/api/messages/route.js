810b2bd74dbe3613f186c5969752e5505d6df69f
4ntisocialband <4ntisocial@gmail.com>
2025-02-23 08:33:15 +0700

    add send, receive message method

--- Enigma_app/src/app/api/messages/route.js
+++ Enigma_app/src/app/api/messages/route.js
@@ -1,35 +1,49 @@
+/* 
+    Message route: Create POST, GET method to handle display and send message between users
+
+    - GET method: Send message to client sides
+        + input: 
+        + output: Response found messeges to client sides
+
+    - POST method: Send message to server side and save in database
+        + input: {user, message}: user info and message they send
+        + output: Response result of write message to database
+
+    - output: 
+*/
+
 import { NextResponse } from "next/server";
 import dbConnect from "@/lib/dbConnect";
-import Message from "@/models/messages";
+import Message from "@/lib/models/messages";
 
 // GET METHOD: Get old messages from database
 const GET = async () => {
     await dbConnect(); // Create connection
-    const messages = await Message.find().sort({timestamp: 1});
-    return NextResponse.json(messages);
+    const messages = await Message.find().sort({timestamp: 1}); // Query for any message
+    return NextResponse.json(messages); // Response found messages
 };
 
 // POST METHOD: Post new messages to database
 const POST = async (req) => {
-    await dbConnect(); // Create connection
+    // Ensure connected to database
+    await dbConnect();
 
     try {
-        // Get body data
+        // Get {username, password}
         const body = await req.json();
-        const { user, message } = body;
+        const { user, message } = body; // Parsing input
 
+        // Response if user or message is not found
         if (!user || !message) {
             return NextResponse.json({ error: "Missing user or message" }, { status: 400 });
         }
 
-        // Save messages to database
-        const newMessage = await Message.create({ user, message });
+        // Save {user, message} to database
+        const newMessage = await Message.create({ user: user, message: message });
 
-        return NextResponse.json(newMessage);
+        return NextResponse.json(newMessage); // Response write to db result
     } catch (error) {
-        // Debugging
-        console.error("Error processing POST request:", error);
-        return NextResponse.json({ error: "Internal server error" }, { status: 500 });
+        return NextResponse.json({ error: "Internal server error" }, { status: 500 }); // Response if error
     }
 };
 
