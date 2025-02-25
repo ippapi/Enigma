6fda47bf30a830af6d782ba4970df1e2461af1ba
4ntisocialband <4ntisocial@gmail.com>
2025-02-25 09:38:42 +0700

    test haha

--- Enigma_app/src/app/page.js
+++ Enigma_app/src/app/page.js
@@ -1,9 +1,11 @@
 "use client";
 
+import Scene from "@/components/threeModelViewer";
+
 export default function Home() {
     return (
         <div>
-            <h1>Welcome to enigma</h1>
+            <Scene />
         </div>
     );
 }
