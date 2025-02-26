02024d3388546cfb9077b4b622bba71c837b467b
4ntisocialband <4ntisocial@gmail.com>
2025-02-26 23:30:07 +0700

    add tarotCard model

---
+++ Enigma_app/src/lib/models/tarotCard.js
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const TarotCardSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    number: { type: String, required: true },
+    arcana: { type: String },
+    suit: { type: String},
+    img: { type: String},
+    fortune_telling: { type: [String]},
+    keywords: { type: [String]},
+    meanings: {
+        light: { type: [String]},
+        shadow: { type: [String]}
+    },
+    Archetype: { type: String},
+    "Hebrew Alphabet": { type: String},
+    Numerology: { type: String},
+    Elemental: { type: String},
+    "Mythical/Spiritual": { type: String},
+    "Questions to Ask": { type: [String]}
+});
+
+export default mongoose.models.TarotCard || mongoose.model("TarotCard", TarotCardSchema);
