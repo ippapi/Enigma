061b740eca5158da08558f555756a329cba71d07
4ntisocialband <4ntisocial@gmail.com>
2025-03-01 11:19:49 +0700

    haha, không đủ kiên nhẫn để làm html css

---
+++ Enigma_app/src/components/footer.js
@@ -0,0 +1,7 @@
+const footer = () => {
+    return (<>
+        giống header, nhưng đây là footer
+    </>);
+};
+
+export default footer;
---
+++ Enigma_app/src/components/navBar.js
@@ -0,0 +1,7 @@
+const navBar = () => {
+    return (<>
+        đây là header nhưng mà không có kiên nhẫn để làm html, css
+    </>);
+};
+
+export default navBar
