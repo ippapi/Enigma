4c0858acc5fc04a0da79b247508447ab7c9b0d11
4ntisocialband <4ntisocial@gmail.com>
2025-03-08 14:30:05 +0700

    change user model

--- Enigma_app/src/lib/models/user.js
+++ Enigma_app/src/lib/models/user.js
@@ -9,6 +9,15 @@ const UserSchema = new mongoose.Schema({
     username: { type: String, required: true, unique: true },
     email: { type: String, required: true, unique: true},
     password: { type: String, required: true },
+    role: {
+        type: String,
+        enum: ["user", "reader", "admin"],
+        default: "user",
+    },
+    profilePicture: {type: String},
+    bio: {type: String},
+    isActive: {type: Boolean, default: true},
+    isOnline: {type: Boolean, default: false},
 }, { timestamps: true });
 
 export default mongoose.models.User || mongoose.model("User", UserSchema);
