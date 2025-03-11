fff3826a18f057dd190660f2c2212b20a0d11b7c
Thao Phuong <22521171@gm.uit.edu.vn>
2025-03-11 22:42:36 +0700

    Update ảnh

--- Enigma_app/public/js/booking.js
+++
@@ -1,8 +0,0 @@
-// Hàm booking
-function booking() {
-    alert("Bạn đã booking thành công!");
-}
-const buttons = document.getElementsByClassName("other_button");
-for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", booking);
-}
--- Enigma_app/public/js/lienhe.js
+++
@@ -1,6 +0,0 @@
-function success() {
-    alert("Đã gửi lời nhắn thành công!");
-}
-
-const button = document.getElementById("success");
-button.addEventListener("click", success);
